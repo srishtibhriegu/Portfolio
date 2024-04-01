5674cc116a2c5b6ad3e8237172415ab896e278c3
srishtibhriegu <srishtibhriegu16@gmail.com>
2024-04-01 15:47:52 +0530

    changes in file

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutLink = document.querySelector('.about-link');
+    var projectLink = document.querySelector('.project-link');
+    var contactlink = document.querySelector('.contact-link');
+    var aboutSection = document.querySelector('.about');
+    var projectSection = document.querySelector('.project');
+    var contactSection = document.querySelector('.contact');
+    
+    aboutLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    projectLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      projectSection.scrollIntoView({ behavior: 'smooth' });
+    });
+
+    contactlink.addEventListener('click', function(event) {
+        event.preventDefault();
+        contactSection.scrollIntoView({ behavior: 'smooth' });
+      });
+  });
