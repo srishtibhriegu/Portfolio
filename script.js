aa031c23bb4ad70826a5ccac5e52b407af9f7b96
srishtibhriegu <srishtibhriegu16@gmail.com>
2024-04-02 13:46:44 +0530

    Final

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var aboutLink = document.querySelector('.about-link');
-    var projectLink = document.querySelector('.project-link');
-    var contactlink = document.querySelector('.contact-link');
-    var aboutSection = document.querySelector('.about');
-    var projectSection = document.querySelector('.project');
-    var contactSection = document.querySelector('.contact');
-    
-    aboutLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      aboutSection.scrollIntoView({ behavior: 'smooth' });
-    });
+  var scrollLinks = document.querySelectorAll('.scroll-link');
 
-    projectLink.addEventListener('click', function(event) {
+  scrollLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
       event.preventDefault();
-      projectSection.scrollIntoView({ behavior: 'smooth' });
+      var targetId = this.getAttribute('href');
+      var targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
     });
-
-    contactlink.addEventListener('click', function(event) {
-        event.preventDefault();
-        contactSection.scrollIntoView({ behavior: 'smooth' });
-      });
   });
+});
 
   function showSidebar(){
     const sidebar = document.querySelector( ".sidebar" )
